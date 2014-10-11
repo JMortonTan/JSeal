73cf83d27f231ec590bd0581f7155b53593d4416
Jonathan Tan <jmtan188@gmail.com>
2014-10-11 17:49:48 -0400

    Added snippets of useful code.

--- Main.java
+++ Main.java
@@ -7,13 +7,31 @@ public class Main
 {
     public static void main(String[] args)
     {
+    }
+     
+    //Sample code for loading image into BufferedImage object.
+    public static void load()
+    {
         BufferedImage img = null;
         try{
             img = ImageIO.read(new File("usa.jpg"));
         } catch (IOException e){
         }
-        
-        
-        
+    }
+    
+    //Sample code for recieving dimension, should break into two when implemented.
+    public static int[][] getDimension(BufferedImage img)
+    {
+        int[][] dimensions = new int[img.getHeight()][img.getWidth()];
+        return(dimensions);
+    }
+    
+    //Sample code for outputting buffered image (modified)
+    public static void save(BufferedImage img)
+    {
+    try {
+        File outFile = new File("save.jpg");
+        ImageIO.write(img, "jpg", outFile);
+    } catch (IOException e){}
     }
 }
---
+++ README.md
@@ -0,0 +1,6 @@
+JSeal
+====================
+
+###About
+
+JSeal is a Java application that allows a user to encode data into an existing .JPEG file.
