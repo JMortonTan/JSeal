e0e223e60e8a86c726404a6c4dc52980cdc069bb
Jonathan Tan <jmtan188@gmail.com>
2014-10-29 19:50:52 -0400

    PixelCoordinate not ready, updated and tweaking.

--- PixelCoordinate.java
+++ PixelCoordinate.java
@@ -1,52 +1,64 @@
-public PixelCoordinate
+public class PixelCoordinate
 {
-	private int xCord,
-				    yCord,
-					argbInt;
+	private int xCord,		//Stores X Coord. value.
+				    yCord;		//Stores Y Coord. value.
 	
-	private int[][] surroundingCord;
+	//TEMPORARY PUBLIC
+	public int[][] surroundingCords = new int[8][8];
 	
+	//Overloaded Constructor
 	public PixelCoordinate(int x, int y)
 	{
 		setX(x);
 		setY(y);
+		setSurroundingCord();
 	}
 	
+	//Set X Coord. value
 	public void setX(int x)
 	{
 		this.xCord = x;
 	}
 	
+	//Set Y Coord. value
 	public void setY(int y)
 	{
 		this.yCord = y;
 	}
 	
+	//Returns X Coord. value
 	public int getX()
 	{
 		return this.xCord;
 	}
 	
+	//Returns Y Coord. value
 	public int getY()
 	{
 		return this.yCord;
 	}
 	
+	//Use Recursion?
+	//Return... int array or store as instance variabe?
+	
+	//THIS IS BROKEN AND NOT THOUGHT OUT WELL.
 	public void setSurroundingCord()
 	{
-		int cordCounter = 0;
-	
-		for(int i = getX() - 1; i <= getX() + 1; i++)
+		int arrayIterator = 0;
+			
+		for(int i = this.getX() - 1; i <= this.getX() + 1; i++)
 		{
-			for int(j = getY() -1; y <= getY() + 1; j++)
+			for (int j = this.getY() -1; j <= this.getY() + 1; j++)
 			{
-				if(i > 0 && j > 0)
+				if(i != this.getX() && j != this.getY())
 				{
-					cordCounter++;
+					this.surroundingCords[arrayIterator][arrayIterator] = i;
+					this.surroundingCords[arrayIterator][arrayIterator + 1] = j;
+					arrayIterator++;
 				}
 			}
 		}
-
-		
 	}
+	
+	
 }
